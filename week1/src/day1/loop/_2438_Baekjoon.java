881ece7c801021559ec94dc7decbb1703c708641
hyeontack5 <sksgusxor@gmail.com>
2021-11-01 22:03:22 +0900

    Add: 반복문 연습

---
+++ week1/src/day1/loop/_2438_Baekjoon.java
@@ -0,0 +1,19 @@
+package day1.loop;
+
+import java.util.Scanner;
+
+public class _2438_Baekjoon {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
