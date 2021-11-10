7a40b08298736310d012a7b908066c118f5bfd9d
hyeontack5 <sksgusxor@gmail.com>
2021-11-10 23:23:41 +0900

    Refactor: 클래스 이름 변경, BufferedReader 사용

---
+++ week1/src/day1/conditional/Baekjoon2753.java
@@ -0,0 +1,18 @@
+package day1.conditional;
+
+import java.util.Scanner;
+
+public class _2753_Baekjoon {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
